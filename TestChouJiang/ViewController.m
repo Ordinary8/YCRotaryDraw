d1883125172f748ae7edf072fc8f2ca3fc675f36
袁灿 <498441403@qq.com>
2015-11-20 11:25:56 +0800

    优化抽奖转盘

--- TestChouJiang/ViewController.m
+++ TestChouJiang/ViewController.m
@@ -10,23 +10,21 @@
 
 @interface ViewController ()
 {
-    float angle;
-    float angleTotal;
-
-    NSInteger num;
+    NSString *strPrise;
 }
 
+@property (retain, nonatomic) UIView  *popView;
+@property (retain, nonatomic) UILabel *labPrise;
+@property (retain, nonatomic) UIButton *btn;
 @property (retain, nonatomic) UIImageView *zhuanpan;
 
 @end
 
-
 @implementation ViewController
+@synthesize btn;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    angle = 10;
     
     //背景
     UIImageView *imgViewBg = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
@@ -45,8 +43,8 @@
     [self.view addSubview:hander];
     
     //开始或停止按钮
-    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake((self.view.frame.size.width-200)/2, self.view.frame.size.height-200, 200, 35)];
-    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    btn = [[UIButton alloc] initWithFrame:CGRectMake((self.view.frame.size.width-200)/2, self.view.frame.size.height-200, 200, 35)];
+    [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
     [btn setTitle:@"开始" forState:UIControlStateNormal];
     [btn setBackgroundColor:[UIColor orangeColor]];
     btn.layer.borderColor = [UIColor orangeColor].CGColor;
@@ -55,103 +53,102 @@
     btn.layer.masksToBounds = YES;
     [self.view addSubview:btn];
     
+    _labPrise = [[UILabel alloc] initWithFrame:CGRectMake(100, 400, 200, 20)];
+    _labPrise.textColor = [UIColor orangeColor];
+    [self.view addSubview:_labPrise];
+    
 }
 
-- (void)btnClick:(UIButton *)btn
+- (void)btnClick
 {
-    switch ([btn tag]) {
-        case 0:
-        {
-            [btn setTitle:@"结束" forState:UIControlStateNormal];
-            [btn setTag:1];
-            
-            CABasicAnimation* rotationAnimation;
-            rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-            rotationAnimation.toValue = [NSNumber numberWithFloat: M_PI * 2.0 ];
-            rotationAnimation.duration = 1.0f;
-            rotationAnimation.cumulative = YES;
-            rotationAnimation.repeatCount = 100;
-            [_zhuanpan.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
-            
-            [self loadData];
-
-            [self startAnimation];
-
-        }
-            break;
-            
-        case 1:
-        {
-            [btn setTitle:@"开始" forState:UIControlStateNormal];
-            [btn setTag:0];
-            
-            [_zhuanpan.layer removeAllAnimations];
-        }
-            break;
-            
-        default:
-            break;
-    }
-}
-
-
-//使用NSURLSessionDataTask请求数据
-- (void)loadData {
+    NSInteger angle;
+    NSInteger randomNum = arc4random()%100;
     
-    // 创建Data Task
-    NSURL *url = [NSURL URLWithString:@"http://192.168.41.48/~jenkins/Json/lottery.php"];
-    
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
-                                                completionHandler:
-                                      ^(NSData *data, NSURLResponse *response, NSError *error) {
-                                          // 输出返回的状态码，请求成功的话为200
-                                          NSDictionary *jsondic;
-                                              jsondic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
-                                          NSLog(@"%@",jsondic);
-                                          
-                                          num = [[[jsondic objectForKey:@"result"] objectForKey:@"num"] integerValue];
-
-                                          [self startAnimation];
-                                          
-                                      }];
-    [dataTask resume];
-}
-
-//旋转
--(void)startAnimation
-{
-    
-    if (num>=91 && num<=99) {
-        angleTotal = 300;
-    } else if (num>=76 && num<= 90) {
-        angleTotal = 240;
-    } else if (num >=51 && num<=75) {
-        angleTotal = 60;
+    if (randomNum>=91 && randomNum<=99) {
+        angle = 300;
+        strPrise = @"一等奖";
+    } else if (randomNum>=76 && randomNum<= 90) {
+        angle = 60;
+        strPrise = @"二等奖";
+    } else if (randomNum >=51 && randomNum<=75) {
+        angle = 180;
+        strPrise = @"三等奖";
     } else {
-        angleTotal = 270;
+        angle = 240;
+        strPrise = @"没中奖";
     }
     
-    angleTotal = angleTotal+360;
+    [btn setTitle:@"抽奖中..." forState:UIControlStateNormal];
+    _labPrise.text = [NSString stringWithFormat:@"中奖结果:%@",@"等待开奖结果"];
+    btn.enabled = NO;
     
-    [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:0.01f];
-    [UIView setAnimationDelegate:self];
-    [UIView setAnimationDidStopSelector:@selector(endAnimation)];
-    _zhuanpan.transform = CGAffineTransformMakeRotation(angleTotal*(M_PI/180.0f));
     
-    [UIView commitAnimations];
+    CABasicAnimation* rotationAnimation;
+    rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    rotationAnimation.toValue = [NSNumber numberWithFloat:angle*M_PI/180 ];
+    rotationAnimation.duration = 1.0f;
+    rotationAnimation.cumulative = YES;
+    rotationAnimation.delegate = self;
+    rotationAnimation.fillMode=kCAFillModeForwards;
+    rotationAnimation.removedOnCompletion = NO;
+    [_zhuanpan.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
 }
 
--(void)endAnimation
+//动画结束
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
 {
+    [UIView animateWithDuration:2.0
+                     animations:^{
+                         
+                         _popView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+                         
+                         _popView.backgroundColor = [UIColor clearColor];
+                         _popView.alpha = 0.7;
+                         
+                         UIImageView *popImageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 150, self.view.frame.size.width-200, self.view.frame.size.width-200)];
+                         popImageView.image = [UIImage imageNamed:@"prise.png"];
+                         [_popView addSubview:popImageView];
+                         [self.view addSubview:_popView];
+                        
+                         _popView.transform = CGAffineTransformMakeScale(1.5, 1.5);
+                         
+                     }
+                     completion:^(BOOL finished) {
+                         [_popView removeFromSuperview];
+                         _labPrise.text = [NSString stringWithFormat:@"中奖结果 : %@",strPrise];
+                         [btn setTitle:@"开始抽奖" forState:UIControlStateNormal];
+                         btn.enabled = YES;
+                         
+                     }];
 
-    angle += 10;
-    
-    if (angle <= angleTotal) {
-        [self startAnimation];
-
-    }
 }
+
+////使用NSURLSessionDataTask请求数据
+//- (void)loadData {
+//
+//    [_activity startAnimating];
+//
+//    // 创建Data Task
+//    NSURL *url = [NSURL URLWithString:@"http://192.168.41.48/~jenkins/Json/lottery.php"];
+//
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    NSURLSession *session = [NSURLSession sharedSession];
+//    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
+//                                                completionHandler:
+//                                      ^(NSData *data, NSURLResponse *response, NSError *error) {
+//                                          // 输出返回的状态码，请求成功的话为200
+//                                          NSDictionary *jsondic;
+//                                              jsondic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+//                                          NSLog(@"%@",jsondic);
+//                                          
+//                                          num = [[[jsondic objectForKey:@"result"] objectForKey:@"num"] integerValue];
+//                                          strPrise = [[jsondic objectForKey:@"result"] objectForKey:@"info"];
+//                                          [_activity stopAnimating];
+//                                          [self startAnimation];
+//                                          
+//                                      }];
+//    [dataTask resume];
+//}
+
+
 @end
